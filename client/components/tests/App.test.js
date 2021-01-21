46cfa0a585994038da6eada4e9b0bca29a0c2553
joelsrubin <joelsrubin@gmail.com>
2021-01-20 18:39:57 -0600

    Nav test passing

--- client/components/tests/App.test.js
+++ client/components/tests/App.test.js
@@ -5,11 +5,18 @@ import { shallow } from "../../enzyme";
 import App from "../App.jsx";
 import GameCarousel from "../GameCarousel.jsx";
 import GameReview from "../GameReview.jsx";
+import Nav from "../Nav.jsx";
+import Modal from "../Modal.jsx"
 
-describe("<App />", () => {
-  it("renders one <GameCarousel /> component", () => {
-    const wrapper = shallow(<GameCarousel />);
-    expect(wrapper.find(GameReview)).to.have.lengthOf(1);
+describe("<Nav />", () => {
+  it('renders children when passed in', () => {
+    const wrapper = shallow((
+      <Nav>
+        <i className="fas fa-caret-down"></i>
+      </Nav>
+    ));
+    expect(wrapper.contains(<i className="fas fa-caret-down"></i>)).to.equal(true);
   });
+
 
 });
